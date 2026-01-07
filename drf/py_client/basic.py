68c9627ad92584ec9bbbb0b6d43b37b25ec015d8
SriTanyaG <sritanya.g-26@scds.saiuniversity.edu.in>
2026-01-07 14:46:40 +0530

    first commit

---
+++ drf/py_client/basic.py
@@ -0,0 +1,18 @@
+import requests
+
+endpoint = "https://httpbin.org/status/200/"
+endpoint = "https://httpbin.org/anything"
+endpoint = "http://127.0.0.1:8000/api/"
+get_response = requests.get(endpoint,json = {"query":"Hello World"}, params = {"abc":123}) # HTTP request
+# print(get_response.text) #Print raw text response
+
+# 1.HTTP request will give HTML response
+# 2.A REST API HTTP Request will give JSON response
+
+# JavaScript Object Notation ~ A python Dict
+
+print(get_response.json())
+
+# {'args': {}, 'data': '', 'files': {}, 'form': {}, 'headers': {'Accept': '*/*', 'Accept-Encoding': 'gzip, deflate', 'Host': 'httpbin.org', 'User-Agent': 'python-requests/2.32.5', 'X-Amzn-Trace-Id': 'Root=1-695e059e-2476e9b33e1ac8fe1fc1d7e2'}, 'json': None, 'method': 'GET', 'origin': '103.16.202.221', 'url': 'https://httpbin.org/anything'}
+
+# print(get_response.status_code)
